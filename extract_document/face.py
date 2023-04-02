30f1e46b72f82580874200f9bd1a3e1ba59a2d2a
rishiraj-58 <s.rishiraj2002@gmail.com>
2023-04-02 09:08:57 +0530

    changed face.py

--- extract_document/face.py
+++ extract_document/face.py
@@ -1,58 +1,58 @@
-# # import cv2
-# # from config import *
-# # import face_recognition
-# # import os
-# # import logging
-# # logger = logging.getLogger("main")
+# import cv2
+# from config import *
+# import face_recognition
+# import os
+# import logging
+# logger = logging.getLogger("main")
 
-# # def detect_faces(image_path):
-# #     """
+# def detect_faces(image_path):
+#     """
 
-# #     :param image_path: input image path for detecting the faces in document
-# #     :return: bool for face detection and documnet image
-# #     """
-# #     # load the jpg file into a numpy aary
-# #     image = face_recognition.load_image_file(image_path)
+#     :param image_path: input image path for detecting the faces in document
+#     :return: bool for face detection and documnet image
+#     """
+#     # load the jpg file into a numpy aary
+#     image = face_recognition.load_image_file(image_path)
 
-# #     # Find all the faces in the image using the default HOG-based model.
-# #     # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-# #     # See also: find_faces_in_picture_cnn.py
-# #     face_locations = face_recognition.face_locations(image)
-# #     logger.debug("[INFO] found {} face(s) In this document.".format(len(face_locations)))
+#     # Find all the faces in the image using the default HOG-based model.
+#     # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
+#     # See also: find_faces_in_picture_cnn.py
+#     face_locations = face_recognition.face_locations(image)
+#     logger.debug("[INFO] found {} face(s) In this document.".format(len(face_locations)))
 
-# #     if len(face_locations) >= 1:
-# #         # extract only first face cord.
-# #         top, right, bottom, left = face_locations[0]
+#     if len(face_locations) >= 1:
+#         # extract only first face cord.
+#         top, right, bottom, left = face_locations[0]
 
-# #         # expand the face location
-# #         top -= 25
-# #         bottom += 25
-# #         left -= 25
-# #         right += 25
+#         # expand the face location
+#         top -= 25
+#         bottom += 25
+#         left -= 25
+#         right += 25
 
-# #         # You can access the actual face itself like this:
-# #         crop_face = image[top:bottom, left:right]
-# #         crop_face = cv2.cvtColor(crop_face, cv2.COLOR_RGB2BGR)
+#         # You can access the actual face itself like this:
+#         crop_face = image[top:bottom, left:right]
+#         crop_face = cv2.cvtColor(crop_face, cv2.COLOR_RGB2BGR)
 
-# #         # write the face
-# #         face_path = os.path.split(image_path)
-# #         face_path = os.path.join(face_path[0], "Faces", "face" + " " + face_path[1])
+#         # write the face
+#         face_path = os.path.split(image_path)
+#         face_path = os.path.join(face_path[0], "Faces", "face" + " " + face_path[1])
 
-# #         # write the crop face to folder
-# #         cv2.imwrite(face_path,  crop_face)
+#         # write the crop face to folder
+#         cv2.imwrite(face_path,  crop_face)
 
-# #         logger.debug("[INFO] Saving crop face to face folder...")
-# #         # convert the color
-# #         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+#         logger.debug("[INFO] Saving crop face to face folder...")
+#         # convert the color
+#         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-# #         # draw the rectangle
-# #         cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 3)
+#         # draw the rectangle
+#         cv2.rectangle(image, (left, top), (right, bottom), (0, 255, 0), 3)
 
-# #         # if face found return the True result
-# #         return True, image
+#         # if face found return the True result
+#         return True, image
 
-# #     else:
-# #         return False, None
+#     else:
+#         return False, None
 # import cv2
 # import os
 # import logging
@@ -110,57 +110,31 @@
 #     else:
 #         return False, None
 
-import face_recognition
-import os
-import logging
-from PIL import Image, ImageDraw
+import insightface
+from PIL import Image
+import cv2
+import numpy as np
 
-logger = logging.getLogger("main")
+# Load the pre-trained model
+detector = insightface.model_zoo.get_model('retinaface_r50_v1')
 
 def detect_faces(image_path):
-    """
+    # Load the image into a numpy array
+    image = Image.open(image_path).convert("RGB")
 
-    :param image_path: input image path for detecting the faces in document
-    :return: bool for face detection and document image
-    """
-    # Load the image
-    image = face_recognition.load_image_file(image_path)
+    # Convert the color
+    image = Image.open(image_path).convert("RGB")
 
-    # Find all the faces in the image
-    face_locations = face_recognition.face_locations(image)
+    # Detect faces in the image
+    detections = detector.detect(image)
 
-    logger.debug("[INFO] found {} face(s) In this document.".format(len(face_locations)))
-
-    if len(face_locations) >= 1:
-        # extract only first face coords.
-        top, right, bottom, left = face_locations[0]
-
-        # expand the face location
-        top -= 25
-        left -= 25
-        bottom += 50
-        right += 50
-
-        # crop the face from the image
-        crop_face = image[top:bottom, left:right]
-
-        # write the face
-        face_path = os.path.split(image_path)
-        face_path = os.path.join(face_path[0], "Faces", "face" + " " + face_path[1])
-
-        # write the cropped face to folder
-        pil_image = Image.fromarray(crop_face)
-        pil_image.save(face_path)
-
-        logger.debug("[INFO] Saving crop face to face folder...")
-
-        # draw the rectangle
-        draw = ImageDraw.Draw(image)
-        draw.rectangle((x, y, x+w, y+h), outline=(0, 255, 0), width=3)
-
-        # if face found return the True result
+    # Draw a rectangle around each face
+    if detections is not None:
+        for det in detections:
+            box = det.bbox.astype(np.int)
+            Image.fromarray(image).draw.rectangle((box[0], box[1], box[2], box[3]), outline=(0, 255, 0), width=2)
         return True, image
-
     else:
         return False, None
+
 
